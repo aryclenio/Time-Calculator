df4d525046ed6f2c3d89d602a61725b4aca4469d
arycleniobarros <arycleniobarros@gmail.com>
2021-10-25 12:06:01 -0300

    🐛  Fix props passing

---
+++ components/MainClock.vue
@@ -0,0 +1,15 @@
+<template>
+  <c-heading>{{time}}</c-heading>
+</template>
+
+<script>
+import { CHeading } from '@chakra-ui/vue';
+
+export default {
+  name: 'MainClock',
+  components: {CHeading},
+  props: {
+    time: String
+  }
+}
+</script>
